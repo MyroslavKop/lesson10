8f9e129f4afed05aef37e55a1abfb05228a4819a
MiroslavKop <kopytovmiroslav@gmail.com>
2022-08-14 20:34:04 +0300

    выполнил задачу №1

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 "use strick";
+
+// Задача №1-------------------------------------------------------------------------/
+
+// class Employee {
+//   static vacationDays = 18;
+//   constructor(firstName, lastName, age, jobPosition, salary) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//     this.jobPosition = jobPosition;
+//     this.salary = salary;
+//   }
+
+//   get fullName() {
+//     console.log(`${this.firstName} ${this.lastName}`);
+//   }
+
+//   yearSalary() {
+//     console.log(this.salary * 12);
+//   }
+// }
+
+// let firstWorker = new Employee("John", "Jameson", 50, "manager", 500);
+// console.log(firstWorker);
+// firstWorker.fullName;
+// firstWorker.yearSalary();
+
+// let secondWorker = new Employee("Miroslav", "Kopitov", 25, "developer", 5000);
+// console.log(secondWorker);
+// secondWorker.fullName;
+// secondWorker.yearSalary();
